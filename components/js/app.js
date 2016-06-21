3a4b85c1b6e3c722133a52b68e23666c976a2d7b
anders-kaffia <anders@godrowsy.com>
2016-06-21 16:55:16 +0200

    Main structure and styling done

--- components/js/app.js
+++ components/js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         e.preventDefault();
         (toggle.classList.contains("is-active") === true) ? toggle.classList.remove("is-active") : toggle.classList.add("is-active");
     });
-    /* Slides open the nav menu */
+    /* Slide open the nav menu */
     $('#bgr').on('click', function(e) {
         e.preventDefault();
         $('.main-nav').toggleClass('menu-open');
@@ -31,6 +31,86 @@ $(document).ready(function () {
         }
     });
 });
+/* ==================================
+            RANDOM BG IMG
+================================== */
+/*
+$(document).ready(function() {
+    var bgImgArray = ['bg-img-cycle1', 'bg-img-cycle2', 'bg-img-cycle3', 'bg-img-cycle4'],
+        randomNumber = Math.floor(Math.random() * bgImgArray.length),
+        randomBg = bgImgArray[randomNumber];
+    $('.container-about').addClass(randomBg);
+});
+*/
+
+/* ==================================
+            CONTACT MODAL
+================================== */
+// reCaptcha
+// $('#contact-submit-btn').on('click', function() {
+//     //e.preventDefault();
+//     $.ajax('https://www.google.com/recaptcha/api/siteverify', {type: "post", data: {
+//         "secret": 6Lc9CCMTAAAAAHQf1Ho-KPhkf95ebzZtWW_WpRK8
+//     }
+//     }).done(function(data) {
+//         if(data.success == true) {
+
+//             // $('#contactform').submit();
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     });    
+// });
+// $('#contact-submit-btn').on('click', function(e) {
+//     e.preventDefault();
+//     return false;
+// });
+
+$(document).ready(function () {
+    if($('body').hasClass('page-template-page-about-me')) {
+        initForm();
+    }
+});
+
+function initForm() {
+    // Based on JavaScript code by Chirp Internet: www.chirp.com.au
+    var modalWrapper = $('#contact-wrapper');
+    var modalWindow  = $('#contact-modal');
+
+    var openModal = function(e) {
+        modalWrapper.addClass('overlay');
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+    };
+    // Removes the class 'overlay' to close 
+    var closeModal = function(e) {
+        modalWrapper.removeClass('overlay');
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+    };
+    // Close when click on the 'overlay' div
+    var clickHandler = function(e) {
+        if(!e.target) e.target = e.srcElement;
+        if(e.target.tagName == "div") {
+            if(e.target.id != "modal_window") closeModal(e);
+        }
+    };
+    // Close the modal if 'ESC' is pressed
+    var keyHandler = function(e) {
+        if(e.keyCode == 27) closeModal(e);
+    };
+
+    if(document.addEventListener) {
+        document.getElementById('modal_open').addEventListener("click", openModal, false);
+        document.getElementById('modal_close').addEventListener("click", closeModal, false);
+        document.addEventListener("click", clickHandler, false);
+        document.addEventListener("keydown", keyHandler, false);
+    } else {
+        $('#modal_open').attachEvent("onclick", openModal);
+        $('#modal_close').attachEvent("onclick", closeModal);
+        document.attachEvent("onclick", clickHandler);
+        document.attachEvent("onkeydown", keyHandler);
+    }
+}
 
 /* ==================================
             WP CUSTOMIZER
